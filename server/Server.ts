d176b6bd8751090865a0d5079078d00ec3602b46
McTom234 <mc_tom234@gmx.de>
2022-10-15 04:16:48 +0200

    Finish server

--- server/Server.ts
+++ server/Server.ts
@@ -12,7 +12,7 @@ export type ServerWithEvents = IOServer<ClientToServerEvents, ServerToClientEven
 export class Server {
 	private static instance: Server;
 	readonly server: ServerWithEvents;
-	private readonly games: Map<String, Game>;
+	readonly games: Map<String, Game>;
 
 	constructor (serverInst: ServerWithEvents) {
 		this.server = serverInst;
@@ -20,66 +20,193 @@ export class Server {
 		Logger.getLogger().info('Server instance created...');
 	}
 
-	public listener (socket: Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>) {
+	public static getInstance (server?: ServerWithEvents): undefined | Server {
+		if (this.instance === undefined && server !== undefined) this.instance = new Server(server);
+		else if (this.instance === undefined) return undefined;
+		return this.instance;
+	}
+
+	public listener (socket: Socket) {
 		Logger.getLogger().silly('Client connected', socket.id);
 
-		socket.on('disconnect', () => {
-			Logger.getLogger().silly('Client disconnected', socket.id);
-			if (socket.data.room) Server.instance.games.get(socket.data.room)?.removePlayer(socket);
-		});
+		socket.on('disconnect', () => Server.instance.onDisconnect(socket));
 
-		socket.on('join room', (room, name) => {
-			if (room === null || room === undefined) {
-				Logger.getLogger().error('Socket tried to connected without room in request data.');
-				return socket.emit('room error');
-			}
-			if (socket.rooms.size !== 0) Array.from(socket.rooms).forEach(room => socket.leave(room));
+		socket.on('join room', (room, name) => Server.instance.onJoinRoom(socket, room, name));
 
-			let game;
-			if ((game = Server.instance.games.get(room))) { // rom exists
-				if (game.addPlayer(socket)) {
-					Server.instance.server.in(room).emit('player joined', name);
-					Logger.getLogger().info('Socket joins room', socket.id, room);
-					socket.join(room);
-					socket.data.room = room;
-					socket.data.name = name;
+		socket.on('choose role', (role) => Server.instance.onChooseRole(socket, role));
 
-					socket.emit('game data', game.getSanitizedGame(socket));
-				} else {
-					Logger.getLogger().error('Socket could not join room', socket.id, room);
-					socket.emit('room error'); // reject play event
+		socket.on('set secret', (pin: 1 | 2 | 3 | 4, color: PinColor | undefined) => Server.instance.onSetSecret(socket, pin, color));
+
+		socket.on('set pin', (pin: 1 | 2 | 3 | 4, color: PinColor | undefined) => Server.instance.onSetPin(socket, pin, color));
+
+		socket.on('set feedback', (pin: 1 | 2 | 3 | 4, color: FeedbackPinColor | undefined) => Server.instance.onSetFeedback(socket, pin, color));
+
+		socket.on('finish turn', () => Server.instance.onFinishTurn(socket));
+	}
+
+	private onDisconnect (socket: Socket) {
+		Logger.getLogger().silly('Client disconnected', socket.id);
+		if (socket.data.room) Server.instance.games.get(socket.data.room)?.round().removePlayer(socket);
+	}
+
+	private onJoinRoom (socket: Socket, room: string, name: string) {
+		const failJoin = () => {
+			Logger.getLogger().error('Socket tried to connected without room in request data.');
+			return socket.emit('room error');
+		};
+
+		// room must not be undefined
+		if (room === null || room === undefined) return failJoin();
+
+		// check socket already joined room
+		if (socket.rooms.size > 1) {
+			const socketRooms = Array.from(socket.rooms);
+			if (socketRooms[0] !== room) socketRooms.forEach(room => socket.leave(room));
+		}
+
+		// get game
+		let game = Server.instance.games.get(room);
+		let round = game?.round();
+		if (!game) { // create room and game
+			Logger.getLogger().info('Create room', room);
+			game = new Game();
+			Server.instance.games.set(room, game);
+		} else if (round!.setter !== undefined) {
+			if (!round!.setRole(socket, Roles.GUESSER)) return failJoin();
+
+			// player joins room with role
+			socket.emit('role assignment', Roles.GUESSER);
+
+			round!.setter.emit('player joined', name);
+		} else if (round!.guesser !== undefined) {
+			if (!round!.setRole(socket, Roles.SETTER)) return failJoin();
+
+			// player joins room with role
+			socket.emit('role assignment', Roles.SETTER);
+
+			round!.guesser.emit('player joined', name);
+		}
+
+		// player joins room
+		Logger.getLogger().info('Socket joins room', socket.id, room);
+		socket.join(room);
+
+		// set client data
+		socket.data.room = room;
+		socket.data.name = name;
+
+		// send client game data
+		socket.emit('game data', ClientGame.fromGame(game, socket.id));
+	}
+
+	private onChooseRole (socket: Socket, role: Roles) {
+		if (socket.rooms.size === 0) return socket.emit('room error');
+
+		const game = Server.instance.games.get(socket.data.room!)!;
+		const otherRole = role === Roles.SETTER ? Roles.GUESSER : Roles.SETTER;
+		const round = game.round();
+
+		if (round.setRole(socket, role)) {
+			socket.emit('role assignment', role);
+			if (round[otherRole] !== undefined && round[otherRole]!.id === socket.id) round[otherRole] = undefined;
+			else if (round[otherRole] === undefined) {
+				const room = Server.instance.server.sockets.adapter.rooms.get(socket.data.room!);
+
+				if (room !== undefined && room.size === 2) {
+					const sndSocket = Server.instance.server.sockets.sockets.get(Array.from(room)
+						.find((id) => id !== socket.id)!);
+
+					if (sndSocket && round.setRole(sndSocket, otherRole)) sndSocket.emit('role assignment', otherRole);
+					else if (sndSocket !== undefined) sndSocket.emit('room error');
 				}
-			} else { // room does not exist
-				// create room and game - let user choose role
-				Logger.getLogger().info('Create room ', room);
-				Server.instance.games.set(room, new Game());
-				game = Server.instance.games.get(room)!;
-				if (!game.addPlayer(socket)) {
-					Logger.getLogger().error('Socket could not join room', socket.id, room);
-					socket.emit('room error'); // reject play event
-					return;
-				}
-				Logger.getLogger().info('Socket joins room', socket.id, room);
-				socket.join(room);
-				socket.data.room = room;
-				socket.data.name = name;
 			}
-		});
+		} else {
+			if (round.setRole(socket, otherRole)) socket.emit('role assignment', otherRole);
+			else return socket.emit('room error');
+		}
 
-		socket.on('choose role', (role) => {
-			if (socket.rooms.size === 0) return socket.emit('room error');
+		// send client game data
+		socket.emit('game data', ClientGame.fromGame(game, socket.id));
+		if (round[otherRole] !== undefined) round[otherRole]!.emit('game data', ClientGame.fromGame(game, round[otherRole]!.id));
+	}
 
-			const game = Server.instance.games.get(socket.data.room!)!;
-			const otherRole = role === 'setter' ? 'guesser' : 'setter';
+	private setPin (socket: Socket, prohibitedRole: Roles) {
+		if (socket.data.room === undefined || !Server.instance.games.has(socket.data.room)) {
+			socket.emit('room error');
+			Logger.getLogger().warn(socket.data.room);
+			return false;
+		}
 
-			if (game.round().setRole(socket, role)) {
-				socket.emit('role assignment', role);
-				if (game.player1 !== undefined && game.player1.id !== socket.id) game.player1.emit('role assignment', otherRole);
-				else if (game.player2 !== undefined && game.player2.id !== socket.id) game.player2.emit('role assignment', otherRole);
+		const game: Game = Server.instance.games.get(socket.data.room)!;
+		if ((socket !== game.round().setter && socket !== game.round().guesser) || socket === game.round()[prohibitedRole]) {
+			socket.emit('room error');
+			Logger.getLogger()
+				.warn(socket === game.round().setter, socket === game.round().guesser, socket === game.round()[prohibitedRole]);
+			return false;
+		}
+
+		return game;
+	}
+
+	private onSetSecret (socket: Socket, pin: 1 | 2 | 3 | 4, color: PinColor | undefined) {
+		const game: boolean | Game = Server.instance.setPin(socket, Roles.GUESSER);
+		if (game instanceof Game) game.round().hiddenSlot[`pin${ pin }`].color = color;
+	}
+
+	private onSetPin (socket: Socket, pin: 1 | 2 | 3 | 4, color: PinColor | undefined) {
+		const game: boolean | Game = Server.instance.setPin(socket, Roles.SETTER);
+		if (!(game instanceof Game)) return;
+
+		game.round().publicSlot()[`pin${ pin }`].color = color;
+
+		Server.instance.server.in(socket.data.room!).emit('set pin', pin, color);
+	}
+
+	private onSetFeedback (socket: Socket, pin: 1 | 2 | 3 | 4, color: FeedbackPinColor | undefined) {
+		const game: boolean | Game = Server.instance.setPin(socket, Roles.GUESSER);
+		if (!(game instanceof Game)) return;
+
+		game.round().publicSlot().feedback[`pin${ pin }`].color = color;
+
+		Server.instance.server.in(socket.data.room!).emit('set feedback', pin, color);
+	}
+
+	private onFinishTurn (socket: Socket) {
+		if (socket.data.room === undefined || !Server.instance.games.has(socket.data.room)) {
+			socket.emit('room error');
+			return false;
+		}
+
+		const game: Game = Server.instance.games.get(socket.data.room)!;
+		if (socket !== game.round().guesser && socket !== game.round().setter) {
+			socket.emit('room error');
+			return false;
+		}
+
+		if (socket.id === game.round().guesser?.id) {
+			if (game.round().publicSlot().setPins && game.round().publicSlot().pinsDefined()) {
+				game.round().publicSlot().setPins = false;
+				game.round().publicSlot().setFeedback = true;
 			} else {
-				if (game.round().setRole(socket, otherRole)) socket.emit('role assignment', otherRole);
-				else socket.emit('room error');
+				socket.emit('room error');
+				return;
 			}
-		});
+		} else if (socket.id === game.round().setter?.id) {
+			if (game.round().publicSlot().setFeedback && game.round().publicSlot().feedback.pinsDefined()) {
+				game.round().publicSlot().setFeedback = false;
+				game.round().publicSlots.push(new Slot());
+			} else if (game.round().hiddenSlot.setPins && game.round().hiddenSlot.pinsDefined()) {
+				game.round().hiddenSlot.setPins = false;
+				game.round().publicSlot().setPins = true;
+			} else {
+				socket.emit('room error');
+				return;
+			}
+		}
+
+		if (game.round().setter !== undefined)
+			game.round().setter!.emit('game data', ClientGame.fromGame(game, game.round().setter!.id));
+		if (game.round().guesser !== undefined)
+			game.round().guesser!.emit('game data', ClientGame.fromGame(game, game.round().guesser!.id));
 	}
 }
--- server/express.ts
+++ server/express.ts
@@ -3,21 +3,20 @@ import { createServer, Server as HttpServer } from 'http';
 import next, { NextApiHandler } from 'next';
 import { Server } from 'socket.io';
 import { Logger } from './Logger';
-import { Server as GameServer } from './Server';
-import { ClientToServerEvents, InterServerEvents, ServerToClientEvents, SocketData } from './SocketTypes';
+import { Server as GameServer, ServerWithEvents } from './Server';
 
 const port: number = parseInt(process.env.PORT || '3000', 10);
-const dev: boolean = process.env.NODE_ENV !== 'production';
+const dev: boolean = process.env.NODE_ENV.trim() !== 'production';
 const nextApp = next({ dev });
 const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(async() => {
 	const app: Express = express();
 	const server: HttpServer = createServer(app);
-	const io: Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData> = new Server();
+	const io: ServerWithEvents = new Server();
 	io.attach(server);
 
-	io.on('connection', GameServer.getInstance(io).listener);
+	io.on('connection', GameServer.getInstance(io)!.listener);
 
 	app.all('*', (req: any, res: any) => nextHandler(req, res));
 
