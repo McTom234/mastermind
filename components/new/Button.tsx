74f6eb3b7a3574ef5b3e8b89e8d06cf0f835104f
McTom234 <mc_tom234@gmx.de>
2023-04-09 00:03:19 -0600

    feat(components): add button

---
+++ components/new/Button.tsx
@@ -0,0 +1,15 @@
+import {ForwardedRef, forwardRef, HTMLProps} from 'react';
+import styles from '../../styles/new/components/button.module.scss';
+import { cn } from '../../lib/helpers';
+
+interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+    type?: 'button' | 'submit' | 'reset';
+    containerClass?: string
+}
+
+const Button = forwardRef(({containerClass, ...props}: ButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+    return <div className={cn(styles['container'], containerClass)}><button {...props}></button></div>;
+})
+Button.displayName = 'Button'
+
+export default Button;
