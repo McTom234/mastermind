ea5ab45122487204523203f3a2fa7873f9e39947
McTom234 <mc_tom234@gmx.de>
2023-04-09 01:42:53 -0600

    feat(pages): add redirect page

---
+++ pages/join/[room]/[name].tsx
@@ -0,0 +1,20 @@
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps<never, {room: string, name: string}> = async ({params}) => {
+  if (!params)
+    throw new Error('Join parameters are not set. Go to /join, to start again. If you have no success, submit a bug report at https://github.com/McTom234/mastermind/issues/new.')
+
+  const searchParams = new URLSearchParams();
+  searchParams.set('room', params.room);
+  searchParams.set('name', params.name);
+  return {
+    redirect: {
+      destination: '/play?' + searchParams.toString(),
+      permanent: true,
+    },
+  };
+};
+
+export default function Page() {
+  return <></>;
+}
