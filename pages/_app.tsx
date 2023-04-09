97763c8b3b6a0a55c55bc46c1b29a1b82cea27da
McTom234 <mc_tom234@gmx.de>
2023-04-09 00:08:20 -0600

    feat(pages): add layout compatibility

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,34 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.scss';
 import {Nunito} from "next/font/google";
+import {NextPage} from "next";
+import {ReactElement, ReactNode} from "react";
 
 const font = Nunito({
 	subsets: ['latin']
 })
 
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout
+}
+
 function MyApp ({
 	Component,
 	pageProps
-}: AppProps) {
+}: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? (page => page);
+
 	return (
 		<div className={font.className}>
 			<Head>
 				<title>MasterMind online multiplayer</title>
 				<meta name="description" content="Famous game mastermind online as multiplayer edition" />
 			</Head>
-			<Component { ...pageProps } />
+			{getLayout(<Component {...pageProps}/>)}
 		</div>
 	);
 }
