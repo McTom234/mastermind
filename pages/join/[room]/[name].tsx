33c6dd2cc434a8c750f618737d3156b940e69e98
McTom234 <mc_tom234@gmx.de>
2023-04-09 01:46:48 -0600

    style(format): run stylelint + prettier

--- pages/join/[room]/[name].tsx
+++ pages/join/[room]/[name].tsx
@@ -1,8 +1,10 @@
 import { GetServerSideProps } from 'next';
 
-export const getServerSideProps: GetServerSideProps<never, {room: string, name: string}> = async ({params}) => {
+export const getServerSideProps: GetServerSideProps<never, { room: string; name: string }> = async ({ params }) => {
   if (!params)
-    throw new Error('Join parameters are not set. Go to /join, to start again. If you have no success, submit a bug report at https://github.com/McTom234/mastermind/issues/new.')
+    throw new Error(
+      'Join parameters are not set. Go to /join, to start again. If you have no success, submit a bug report at https://github.com/McTom234/mastermind/issues/new.'
+    );
 
   const searchParams = new URLSearchParams();
   searchParams.set('room', params.room);
