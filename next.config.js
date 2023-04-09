9902d7b83cb2b3a1d0079c3f19cf33601da604cf
McTom234 <mc_tom234@gmx.de>
2023-04-09 01:09:17 -0600

    feat(next): add stylelint conditionally to config file

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   swcMinify: true,
 }
 
+if (process.env.ENV === 'development') {
+ import("stylelint-webpack-plugin").then((StylelintWebpackPlugin) => {
+   nextConfig.webpack = (config) => {
+     config.plugins.push(new StylelintWebpackPlugin());
+     return config;
+   }
+ });
+}
+
 export default nextConfig;
