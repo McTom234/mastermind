72ebf03e039c2eee886f8d3f24adbb9502ac4db3
McTom234 <mc_tom234@gmx.de>
2023-04-10 19:00:22 -0600

    feat(play): move main part to component
    cleans the play page file

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,19 +1,13 @@
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Board from 'src/components/board';
-import ColorSelector from 'src/components/colorSelector';
-import { currentPublicSlotOfGame, currentRoundOfGame, pinsDefined } from 'src/components/helpers';
+import { currentPublicSlotOfGame } from 'src/components/helpers';
 import LoadingScreen from 'src/components/new/LoadingScreen';
-import RoleSelector from 'src/components/new/RoleSelector';
+import PlayScreen from 'src/components/new/PlayScreen';
+import RoleSelectionScreen from 'src/components/new/RoleSelectionScreen';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
 import { ClientGame } from 'server/models/Game';
-import { PinColor } from 'server/models/Pin';
 import { ClientToServerEvents, Roles, ServerToClientEvents } from 'server/SocketTypes';
 import { io, Socket } from 'socket.io-client';
-import colorStyles from 'src/styles/Color.module.sass';
-import styles from 'src/styles/Play.module.sass';
 
 const Play: NextPage = () => {
   const router = useRouter();
@@ -23,43 +17,23 @@ const Play: NextPage = () => {
 
   // game related vars
   const [role, setRole] = useState<Roles>();
-  const [game, setGame] = useState<ClientGame>();
-  const hiddenSlot = game ? currentRoundOfGame(game).hiddenSlot : undefined;
+  const game = useRef<ClientGame>();
 
   // client related vars
   const loadingText = useRef('Connecting...');
   const [roleSelection, setRoleSelection] = useState(true);
-  const [selectedColor, selectColor] = useState<PinColor>();
-  const editable = useRef(false);
-  const canFinish = useRef(false);
 
-  if (game !== undefined) {
-    const slot = currentPublicSlotOfGame(game);
-
-    // editable
-    if (role === Roles.GUESSER && slot.setPins) editable.current = true;
-    else if (role === Roles.SETTER) {
-      if (slot.setFeedback) editable.current = true;
-      else editable.current = hiddenSlot !== undefined && hiddenSlot.setPins;
-    } else editable.current = false;
-
-    // canFinish
-    if (role === Roles.GUESSER && slot !== undefined && pinsDefined(slot) && slot.setPins) canFinish.current = true;
-    else if (role === Roles.SETTER) {
-      if (slot !== undefined && slot.setFeedback) canFinish.current = true;
-      else canFinish.current = hiddenSlot !== undefined && pinsDefined(hiddenSlot) && hiddenSlot.setPins;
-    } else canFinish.current = false;
-  } else {
-    editable.current = false;
-    canFinish.current = false;
+  function join() {
+    if (router.query.room && router.query.name) {
+      console.log('conjoin', router.query);
+      loadingText.current = 'Waiting for the Server...';
+      socketRef.current.emit('join room', String(router.query.room), String(router.query.name));
+    }
   }
 
-  function updateGame() {
-    const g = {};
-    Object.assign(g, game);
-    setGame(g as ClientGame);
-  }
-
+  socketRef.current.on('connect', () => {
+    join();
+  });
   socketRef.current.on('room error', () => {
     console.error('server-side room error');
     socketRef.current.disconnect();
@@ -68,127 +42,44 @@ const Play: NextPage = () => {
     });
   });
   socketRef.current.on('player joined', (name) => {
-    if (game === undefined) return;
-    game.playerName = name;
-    updateGame();
+    if (game.current === undefined) return;
+    game.current.playerName = name;
   });
-  socketRef.current.on('game data', (game) => {
-    setGame(game);
+  socketRef.current.on('game data', (gameData) => {
+    game.current = gameData;
   });
   socketRef.current.on('role assignment', (role) => {
+    console.log('role');
     setRole(role);
   });
   socketRef.current.on('set pin', (pin, color) => {
-    if (game === undefined) return;
-    currentPublicSlotOfGame(game)[`pin${pin}`].color = color;
-    updateGame();
+    if (game.current === undefined) return;
+    currentPublicSlotOfGame(game.current)[`pin${pin}`].color = color;
   });
   socketRef.current.on('set feedback', (pin, color) => {
-    if (game === undefined) return;
-    currentPublicSlotOfGame(game).feedback[`pin${pin}`].color = color;
-    updateGame();
+    if (game.current === undefined) return;
+    currentPublicSlotOfGame(game.current).feedback[`pin${pin}`].color = color;
   });
-
   socketRef.current.on('disconnect', () => (loadingText.current = 'Connecting...'));
 
   // on browser load
   useEffect(() => {
     socketRef.current.connect();
   }, []);
-  useEffect(() => {
-    function join() {
-      loadingText.current = 'Waiting for the Server...';
-      socketRef.current.emit('join room', String(router.query.room), String(router.query.name));
-    }
 
-    if (router.query.room !== undefined && router.query.name !== undefined) {
-      if (socketRef.current.connected) {
-        join();
-      } else {
-        socketRef.current.on('connect', () => {
-          join();
-        });
-      }
-    }
-  }, [router.query]);
-
-  function setRoleCallback(role: Roles) {
-    if (role === Roles.GUESSER || role === Roles.SETTER) {
-      setRoleSelection(false);
-      socketRef.current.emit('choose role', role);
-    }
-  }
-
-  function setPinCallback(pin: 1 | 2 | 3 | 4) {
-    if (pin < 1 && pin > 4) return undefined;
-    socketRef.current.emit('set pin', pin, selectedColor);
-    if (game === undefined) return undefined;
-    currentPublicSlotOfGame(game)[`pin${pin}`].color = selectedColor;
-    updateGame();
-    return selectedColor;
-  }
-
-  function setFeedbackCallback(pin: 1 | 2 | 3 | 4) {
-    if (pin < 1 && pin > 4) return undefined;
-    if (selectedColor !== 'white' && selectedColor !== 'red') {
-      socketRef.current.emit('set feedback', pin, undefined);
-      if (game === undefined) return undefined;
-      currentPublicSlotOfGame(game).feedback[`pin${pin}`].color = undefined;
-      updateGame();
-      return undefined;
-    }
-    socketRef.current.emit('set feedback', pin, selectedColor);
-    if (game === undefined) return undefined;
-    currentPublicSlotOfGame(game).feedback[`pin${pin}`].color = selectedColor;
-    updateGame();
-    return selectedColor;
-  }
-
-  function setSecretCallback(pin: 1 | 2 | 3 | 4) {
-    if (pin < 1 && pin > 4) return undefined;
-    socketRef.current.emit('set secret', pin, selectedColor);
-    if (!game || !hiddenSlot) return undefined;
-    hiddenSlot[`pin${pin}`].color = selectedColor;
-    updateGame();
-    return selectedColor;
-  }
-
-  return (
-    <div className={styles.fullHeight}>
-      {role && socketRef.current.connected ? (
-        <>
-          <Board game={game} setSecretCallback={setSecretCallback} setPinCallback={setPinCallback} role={role} setFeedbackCallback={setFeedbackCallback} />
-          <ColorSelector
-            selectedColor={selectedColor}
-            selectColor={selectColor}
-            feedbackOnly={!game || (role === 'setter' && currentRoundOfGame(game).hiddenSlot !== undefined && !(hiddenSlot ? hiddenSlot.setPins : false))}
-          >
-            <FontAwesomeIcon
-              className={`${colorStyles.colorSelectorButton} ${styles.confirm} ${
-                editable.current ? (canFinish.current ? '' : styles.invalid) : styles.noSelect
-              }`}
-              icon={faCheckCircle}
-              onClick={() => {
-                if (canFinish.current) {
-                  if (game !== undefined) {
-                    if (hiddenSlot) hiddenSlot.setPins = false;
-                    currentPublicSlotOfGame(game).setPins = false;
-                    currentPublicSlotOfGame(game).setFeedback = false;
-                    updateGame();
-                  }
-                  socketRef.current.emit('finish turn');
-                }
-              }}
-            />
-          </ColorSelector>
-        </>
-      ) : roleSelection && socketRef.current.connected ? (
-        <RoleSelector setRole={setRoleCallback} />
-      ) : (
-        <LoadingScreen text={loadingText.current} />
-      )}
-    </div>
-  );
+  if (role && socketRef.current.connected) return <PlayScreen game={game} role={role} socket={socketRef.current} />;
+  if (roleSelection && socketRef.current.connected)
+    return (
+      <RoleSelectionScreen
+        setRole={(role: Roles) => {
+          if (role === Roles.GUESSER || role === Roles.SETTER) {
+            setRoleSelection(false);
+            socketRef.current.emit('choose role', role);
+          }
+        }}
+      />
+    );
+  return <LoadingScreen text={loadingText.current} />;
 };
 
 export default Play;
